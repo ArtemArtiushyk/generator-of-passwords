db00ffef670bcea0fe32d500853cd5545b4d1fa0
ArtemArtiushyk <artemart2010.ar@gmail.com>
2025-03-17 17:42:02 +0100

    1.0.1

--- generator.py
+++ generator.py
@@ -1,38 +1,31 @@
 import random
 import string
 
-try:
 
-    def generate(length: int):
+def generate():
+    try:
+        length = int(input("Length of your new password: "))
         characters = string.ascii_letters + string.digits
         password = ""
+
         for i in range(length):
             password += random.choice(characters)
         print(
             f"""
-Your new generated password:
-{password}
-"""
+            Your new generated password:
+            {password}
+            """
         )
+    except ValueError:
+        print(
+            """
+            >>>> Length shouldn't contain any other symbols except numbers.
+            >>>> Length must be a whole number.
+            >>>> Length shouldn't be empty.
+            """
+        )
+    except KeyboardInterrupt:
+        print("\n\n>>>> You quitted. Goodbye!\n")
 
-    leng = int(input("Length of your new password: "))
-    generate(leng)
 
-except ValueError:
-    print(
-        """
->>>> Length shouldn't contain any other symbols except numbers.
->>>> Length must be a whole number.
->>>> Length shouldn't be empty.
-"""
-    )
-
-except KeyboardInterrupt:
-    print(
-        """
-
->>>> You quitted. Goodbye!
-"""
-    )
-
-# TODO: fix SR
+generate()
